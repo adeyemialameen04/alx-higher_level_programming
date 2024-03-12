8e535ebe3e7b9b058d5b272d7a1eb3d89d410259
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-13 00:26:43 +0100

    edited 5-comb

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-for num in range(100):
-    if num < 10:
-        print("0{:d}".format(num), end=", " if num != 99 else "\n")
+for i in range(100):
+    print("{:02d}".format(i), end="")
+    if i != 99:
+        print(", ", end="")
     else:
-        print("{:d}".format(num), end=", " if num != 99 else "\n")
+        print("{}".format(""))
