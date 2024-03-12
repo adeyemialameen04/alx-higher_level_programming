7752424a41fe0bcb03033ac1d5676d86a2d766a3
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-03-12 07:37:24 -0700

    test upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for c in str:
+        if 'a' <= c <= 'z':
+            upper_ch = chr(ord(c) - ord('a') + ord('A'))
+            print("{}".format(upper_ch), end='')
+        else:
+            print("{}".format(c), end='')
+
+        print()
