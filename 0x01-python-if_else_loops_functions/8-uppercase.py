dbe738ebb30bf5b0c6905722fc19e565b06aa467
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-03-12 07:45:48 -0700

    fixed newline issue

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def uppercase(str):
+    length = len(str)
+
     for c in str:
         if 'a' <= c <= 'z':
             upper_ch = chr(ord(c) - ord('a') + ord('A'))
-            print("{}".format(upper_ch), end='')
+            print("{}".format(upper_ch), end='' if i < length - 1 else '\n')
         else:
-            print("{}".format(c), end='')
-        print("{}".format(""))
+            print("{}".format(c), end='' if i < length - 1 else '\n')
