489c3acdce55cb9a18f62f7960651dff4dad646e
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-14 14:51:06 +0100

    yare yare

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     total_number_of_args = len(sys.argv)
     result = 0
 
-    for i in range(1, total_number_of_args):
-        result = int(sys.argv[i]) + result
-
+    sys.argv.pop(0)
+    for item in sys.argv:
+        result += int(item)
 
     print("{:d}".format(result))
