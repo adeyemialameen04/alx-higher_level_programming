5a961adebbda34fee4704e2334cc1b2e18c58146
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-14 13:32:10 +0100

    testing args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-import sys
 
-total_no_of_args = len(sys.argv) - 1
+if __name__ == "__main__":
+    import sys
 
-if len == 1:
-    print("1 argument:")
-else:
-    print(f"{total_no_of_args} arguments")
+    total_no_of_args = len(sys.argv)
+
+    if len == 1:
+        print("1 argument:")
+    else:
+        print(f"{total_no_of_args - 1} arguments")
+
+    for i in range(1, total_no_of_args):
+        print(f"{i}: {sys.argv[i]}")
