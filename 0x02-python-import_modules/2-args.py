5e5cc824c4024b32675aa9398e358727e0046257
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-14 15:11:42 +0100

    rubbish

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-# 2-args.py
 
 if __name__ == "__main__":
     import sys
 
-    if (len(sys.argv) - 1) == 0:
-        print("{:d} arguments.".format(len(sys.argv) - 1))
-    elif (len(sys.argv) - 1) == 1:
-        print("{:d} argument:".format(len(sys.argv) - 1))
+    total_no_of_args = len(sys.argv)
+
+    if total_no_of_args == 1:
+        print("{:d} arguments.".format(0))
+    elif total_no_of_args == 2:
+        print("{:d} argument:".format(1))
     else:
-        print("{:d} arguments:".format(len(sys.argv) - 1))
-    for i in range(len(sys.argv) - 1):
-        print("{:d}: {}".format(i + 1, sys.argv[i+1]))
+        print("{:d} arguments:".format(total_no_of_args - 1))
+
+    for i in range(1, total_no_of_args):
+        print("{:d}: {}".format(i, sys.argv[i]))
