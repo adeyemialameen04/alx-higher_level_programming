7ff08e4ba20debf3ac7cc5d65cfeb5a4b675253a
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-14 13:41:40 +0100

    testing args fix again

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,12 @@ if __name__ == "__main__":
 
     total_no_of_args = len(sys.argv)
 
-    if len == 1:
+    if total_no_of_args == 1:
+        print("0 argument.")
+    elif total_no_of_args == 2:
         print("1 argument:")
     else:
-        print(f"{total_no_of_args - 1} arguments")
+        print(f"{total_no_of_args - 1} arguments:")
 
     for i in range(1, total_no_of_args):
         print(f"{i}: {sys.argv[i]}")
