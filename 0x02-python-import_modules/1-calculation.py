13fe7358ba61cdbe26dad71976470a07647f06f6
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-14 12:59:27 +0100

    task 2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,10 @@
 import calculator_1 as calculator_var
 
-a = 10
-b = 5
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-print("{} + {} = {}".format(a, b, calculator_var.add(a, b)))
-print("{} - {} = {}".format(a, b, calculator_var.sub(a, b)))
-print("{} * {} = {}".format(a, b, calculator_var.mul(a, b)))
-print("{} / {} = {}".format(a, b, calculator_var.div(a, b)))
+    print("{} + {} = {}".format(a, b, calculator_var.add(a, b)))
+    print("{} - {} = {}".format(a, b, calculator_var.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calculator_var.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calculator_var.div(a, b)))
