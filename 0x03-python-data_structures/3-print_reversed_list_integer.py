40932f26b9bf1c8682e1411d7940fc21560f18ea
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-16 13:59:17 +0100

    removed rev

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
-def print_reversed_list_integer(my_list=[]):
+def print_reversed_list_integer(my_list):
 
-    if len(my_list) != 0:
-        for num in reversed(my_list):
-            print("{:d}".format(num))
-    else:
-        print("{:d}".format(0))
+    for num in reversed(my_list):
+        print("{:d}".format(num))
