e6935dbeec0e479a50ff14b19fa35053509ea41a
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-16 14:30:42 +0100

    changed to if, elif

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0:
+        return my_list.copy()
+    elif idx >= len(my_list):
         return my_list.copy()
     else:
         new_list = my_list[:idx] + my_list[idx+1:]
