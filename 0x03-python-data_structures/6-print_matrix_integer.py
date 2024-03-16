1b8887d60b12bddf616263494474b908561f2974
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-16 14:22:03 +0100

    ffixed matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    matrix_len = len(matrix)
+    for row in matrix:
+        for num in row:
+            print("{:d}".format(num), end=" ")
+        print()
 
-    if matrix_len > 1:
-        for i in matrix:
-            print(i)
