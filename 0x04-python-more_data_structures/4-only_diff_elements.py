2b6c1ddbf775acd802a0602270915ca4ea293a8c
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-20 14:46:04 +0100

    fixed 4 difference

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    new_set = set_1 - set_2
+    diff_set_1 = set_1 - set_2
+    diff_set_2 = set_2 - set_1
+
+    new_set = diff_set_1 | diff_set_2
 
     return new_set
