1eff16c50eead1595ab0374320819d390e2f202a
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-03-20 16:34:31 +0100

    roman numeral

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
+
+    roman_values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for symbol in reversed(roman_string):
+        value = roman_values.get(symbol)
+        if not value:
+            return 0
+
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+
+        prev_value = value
+
+    return total
