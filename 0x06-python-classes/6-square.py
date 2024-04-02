401352fd92c7244ba47743ac31ae4a05bcfccce0
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-03 00:38:32 +0100

    fixed print in 6-square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -95,5 +95,9 @@ class Square:
             print()
             return
 
+        for i in range(self.__position[1]):
+            print()
+
         for _ in range(self.__size):
+            print(" " * self.__position[0], end="")
             print("#" * self.__size)
