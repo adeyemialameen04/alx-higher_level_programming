bd1d7f2b432710c184a909d91549268457ed6b79
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-02 13:12:25 +0100

    removed outer try/except

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,15 +2,12 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     printed = 0
-    try:
-        for i in range(x):
-            try:
-                print("{:d}".format(my_list[i]), end='')
-                printed += 1
-            except (ValueError, TypeError):
-                pass
-    except IndexError:
-        pass
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            printed += 1
+        except (ValueError, TypeError):
+            pass
 
     print()
     return printed
