554e356988cc555655b05c32c17b553504bf605b
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-02 13:09:22 +0100

    changed the pos of typeerror

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,9 +7,9 @@ def safe_print_list_integers(my_list=[], x=0):
             try:
                 print("{:d}".format(my_list[i]), end='')
                 printed += 1
-            except ValueError:
+            except (ValueError, TypeError):
                 pass
-    except (IndexError, TypeError):
+    except IndexError:
         pass
 
     print()
