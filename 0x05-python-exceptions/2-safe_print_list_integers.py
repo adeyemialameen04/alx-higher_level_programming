cd4cc7e903729446a36c7745b9021019b516a362
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-02 13:03:28 +0100

    added try except in 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,9 +4,12 @@ def safe_print_list_integers(my_list=[], x=0):
     printed = 0
     try:
         for i in range(x):
-            print(f"{my_list[i]:d}", end='')
-            printed += 1
-    except (IndexError, TypeError, ValueError):
+            try:
+                print(f"{my_list[i]:d}", end='')
+                printed += 1
+            except ValueError:
+                pass
+    except IndexError:
         pass
 
     print()
