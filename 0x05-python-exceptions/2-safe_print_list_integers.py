abe6c7a3f1eee425f5d1dfb80e129fb7a5aa2588
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-02 13:06:25 +0100

    changed to .str(format)

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for i in range(x):
             try:
-                print(f"{my_list[i]:d}", end='')
+                print("{:d}".format(my_list[i]), end='')
                 printed += 1
             except ValueError:
                 pass
