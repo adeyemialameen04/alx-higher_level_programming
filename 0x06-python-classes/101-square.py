159a68df4485cb9b787347c8ed19f74c304b1a6f
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-03 02:58:28 +0100

    testing new

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -95,15 +95,15 @@ class Square:
         Returns:
             Nothing.
         """
-        if self.size == 0:
-            print()
+        if self.__size == 0:
+            print("")
             return
 
-        for _ in range(self.position[1]):
-            print()
-
-        for _ in range(self.size):
-            print(" " * self.position[0] + "#" * self.size)
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
 
     def __str__(self):
         """
@@ -111,5 +111,11 @@ class Square:
         Returns:
             An empty str.
         """
-        self.my_print()
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
         return ""
