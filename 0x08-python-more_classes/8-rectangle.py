dd6b20534d73fd0ee42e65014bf70965e349a917
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-08 14:33:59 +0100

    testing static

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -114,7 +114,16 @@ class Rectangle:
         """
         return f"Rectangle({self.__width}, {self.__height})"
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """
+        Bigger or equal.
+        Args:
+            rect_1: The first rectangle.
+            rect_2: The second rectangle.
+        Returns:
+            The bigger rectangle.
+        """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
 
