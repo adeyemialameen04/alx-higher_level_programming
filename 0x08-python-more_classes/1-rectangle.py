9ae35b19412dd02383fad867a2d0f56c79e9a54c
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-08 13:09:48 +0100

    added checks in init back

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,6 +15,16 @@ class Rectangle:
             width: The width of the rectangle
             height: The height of the rectangle
         """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
         self.__width = width
         self.__height = height
 
