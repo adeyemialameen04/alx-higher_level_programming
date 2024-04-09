19965b44686ef5bf0b65c2b42d23505e848346c9
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-09 22:59:37 +0100

    added docstrings and perms

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""Documenting module"""
+
 
 def magic_string():
+    """
+    Prints a string.
+    Returns:
+        Nothing.
+    """
     magic_string.n = getattr(magic_string, 'n', 0) + 1
     return "BestSchool" + ", BestSchool" * (magic_string.n - 1)
