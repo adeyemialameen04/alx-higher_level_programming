b686456e0a4731e13a0532691e7abf71adf47d18
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-10 00:46:15 +0100

    removed pychache lets test again

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Matrix multiply
+"""
+
+
+def matrix_mul(m_a, m_b):
+    """
+    Multiply two matrices
+    Args:
+        m_a: The first matrix.
+        m_b: The second matrix.
+    Returns:
+        The product.
+    """
+    if not isinstance(m_a, list) or not isinstance(m_b, list):
+        raise TypeError("m_a must be a list or m_b must be a list")
+
+    if not all(isinstance(row, list) for row in m_a) or not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_a must be a list of lists or m_b must be a list of lists")
+
+    if not m_a or not m_b:
+        raise ValueError("m_a can't be empty or m_b can't be empty")
+
+    if not all(isinstance(element, (int, float)) for row in m_a for element in row) or not all(
+            isinstance(element, (int, float)) for row in m_b for element in row):
+        raise TypeError("m_a should contain only integers or floats or m_b should contain only integers or floats")
+
+    if not all(len(row) == len(m_a[0]) for row in m_a) or not all(len(row) == len(m_b[0]) for row in m_b):
+        raise TypeError("each row of m_a must be of the same size or each row of m_b must be of the same size")
+
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    result = []
+    for i in range(len(m_a)):
+        row = []
+        for j in range(len(m_b[0])):
+            element = sum(m_a[i][k] * m_b[k][j] for k in range(len(m_b)))
+            row.append(element)
+        result.append(row)
+    return result
