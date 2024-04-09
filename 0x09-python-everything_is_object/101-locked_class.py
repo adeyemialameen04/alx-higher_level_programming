dfb43cacca79e027472816261799e89b1b204cc3
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-09 23:59:36 +0100

    added docstrings

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
+"""Documenting module"""
+
 
 class LockedClass:
+    """
+    Locked class.
+    """
     def __setattr__(self, name, value):
+        """
+        Sets the attribute.
+        Args:
+            name: name of the attr.
+            value: the value of the attr.
+        Returns:
+            Nothing
+        """
         if name != 'first_name':
             raise AttributeError(f"'LockedClass' object has no"
                                  f" attribute '{name}'")
