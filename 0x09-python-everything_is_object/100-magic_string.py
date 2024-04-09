779a59ecf77ba1ed16ece72b5eb0f24e362df98f
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-09 23:02:35 +0100

    made it 4lines

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-"""Documenting module"""
-
-
 def magic_string():
-    """
-    Prints a string.
-    Returns:
-        Nothing.
-    """
     magic_string.n = getattr(magic_string, 'n', 0) + 1
     return "BestSchool" + ", BestSchool" * (magic_string.n - 1)
