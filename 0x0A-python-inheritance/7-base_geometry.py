580fb851a0525fb79000efb34de9514c2cf241c7
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-15 15:04:43 +0100

    fixed static method

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -17,8 +17,7 @@ class BaseGeometry:
         """
         raise Exception("area() is not implemented")
 
-    @staticmethod
-    def integer_validator(name, value):
+    def integer_validator(self, name, value):
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
