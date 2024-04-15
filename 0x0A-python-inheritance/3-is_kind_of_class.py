c954d5b0b6199a3e0203223302f30dda8750aecd
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-15 14:17:39 +0100

    is instance 2

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Is instance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Is instance.
+    Args:
+        obj: The obj
+        a_class: The class
+    Returns:
+        A boolean
+    """
+    return isinstance(obj, a_class)
