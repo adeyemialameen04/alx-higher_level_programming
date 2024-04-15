fa37f96d7d3a0d334190c28c753a7afc867cd5ff
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-15 14:45:57 +0100

    area geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Documenting module"""
+
+
+class BaseGeometry:
+    """
+    Documenting class.
+    """
+    def __init__(self):
+        pass
+
+    def area(self):
+        """
+        Raise an exception.
+        Returns:
+            Nothing.
+        """
+        raise Exception("area() is not implemented")
