bea9413da3749ee91e2b325f1ffe1b271fff5620
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-15 16:55:29 +0100

    documented class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Documenting module"""
-
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """
+    Documenting class.
+    """
     def __init__(self, width, height):
         self.__width = 0
         self.__height = 0
