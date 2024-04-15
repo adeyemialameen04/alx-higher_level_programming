b02c69019a53c2a9bb2dd64eb1849a12ddd08134
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-15 20:08:34 +0100

    new set of tests

--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -4,8 +4,8 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 bg = BaseGeometry()
 
 bg.integer_validator("my_int", 12)
-bg.integer_validator("width", 89)
-bg.integer_validator(20)
+bg.integer_validator("width", True)
+# bg.integer_validator(20)
 
 # >>> bg.integer_validator(20)
 # Traceback (most recent call last):
