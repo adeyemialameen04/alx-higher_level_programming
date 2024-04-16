a9813541dcbc62899b0640fb2465319cb5c48f4b
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-16 16:35:29 +0100

    added docstrings

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,14 @@
 
 
 def write_file(filename="", text=""):
+    """
+    Writes to a file.
+    Args:
+        filename: The name of the file.
+        text: The txt to be written.
+    Returns:
+        The length of what was written.
+    """
     with open(filename, "w") as file:
         file.write(text)
         return len(text)
