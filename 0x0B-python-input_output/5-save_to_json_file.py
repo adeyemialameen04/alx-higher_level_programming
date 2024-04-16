67d3468e70fb3da1442efcdd92d9691f1c9cce77
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-16 17:06:51 +0100

    yare yare 2

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,6 +4,14 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    Saves a python dict to a json file.
+    Args:
+        my_obj: The obj to be converted and saved.
+        filename: The file it should be saved to.
+    Returns:
+        Nothing.
+    """
     with open(filename, "w") as file:
         data = json.dumps(my_obj)
         file.write(data)
---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Documenting module"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Loads a python dict from a json file.
+    Args:
+        filename: aThe fiilename to be read.
+    Returns:
+        Nothing.
+    """
+    with open(filename, "r") as file:
+        data = json.loads(filename)
+        return data
