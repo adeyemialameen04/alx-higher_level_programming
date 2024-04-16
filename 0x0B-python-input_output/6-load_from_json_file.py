591c167d9041931d1c13f0b381d1db683e18b363
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-16 17:16:08 +0100

    mistake lol

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -12,5 +12,5 @@ def load_from_json_file(filename):
         Nothing.
     """
     with open(filename, "r", encoding="utf-8") as file:
-        data = json.load(filename)
+        data = json.load(file)
     return data
