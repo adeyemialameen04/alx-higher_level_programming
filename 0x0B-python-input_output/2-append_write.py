9a8fdecc2fb0d7460222253f284e03e42548032f
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-16 16:41:30 +0100

    Append

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Documenting module"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends to a file.
+    Args:
+        filename: The file to append to
+        text: The text to append.
+    Returns:
+        Nothing
+    """
+    with open(filename, "a", "utf-8") as file:
+        file.write(text)
+        return len(text)
