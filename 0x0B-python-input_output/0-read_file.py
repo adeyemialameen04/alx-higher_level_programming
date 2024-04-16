cfd9b441483eb7882b3f9ae0fffdb93ee5ecd206
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-16 16:02:47 +0100

    work ffs

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,13 @@
 
 
 def read_file(filename=""):
+    """
+    Reads a file and prints its data.
+    Args:
+        filename: The file to be read.
+    Returns:
+        Nothing.
+    """
     with open(filename, encoding="utf-8") as file:
         data = file.read()
         print(data)
