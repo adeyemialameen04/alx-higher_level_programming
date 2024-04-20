eaacb01c7b941488afdc87a735fe77b7d4ba5a4b
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-20 18:11:26 +0100

    again

--- 0x0C-python-almost_a_circle/main.py
+++ 0x0C-python-almost_a_circle/main.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-""" 17-main """
+""" 16-main """
 from models.rectangle import Rectangle
 
 if __name__ == "__main__":
 
-    r1 = Rectangle(3, 5, 1)
-    r1_dictionary = r1.to_dictionary()
-    r2 = Rectangle.create(**r1_dictionary)
-    print(r1)
-    print(r2)
-    print(r1 is r2)
-    print(r1 == r2)
+    list_input = [
+        {'id': 89, 'width': 10, 'height': 4},
+        {'id': 7, 'width': 1, 'height': 7}
+    ]
+    json_list_input = Rectangle.to_json_string(list_input)
+    list_output = Rectangle.from_json_string(json_list_input)
+    print("[{}] {}".format(type(list_input), list_input))
+    print("[{}] {}".format(type(json_list_input), json_list_input))
+    print("[{}] {}".format(type(list_output), list_output))
