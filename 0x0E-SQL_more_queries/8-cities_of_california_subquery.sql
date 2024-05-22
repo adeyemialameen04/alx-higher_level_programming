1b43faabecbd956a6ac84856ac136cc29ffe4fd4
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-05-22 19:42:56 +0100

    fix

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Subqueries.
 SELECT id, name
 FROM states
-WHERE state_id = (SELECT id from states WHERE name = "California");
+WHERE state_id = (SELECT id FROM states WHERE name = "California");
