8fbc88c256a1c27e5614cd0b290436dbb943a23b
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-05-22 19:33:29 +0100

    now?

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- Cities.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+CREATE TABLE IF NOT EXISTS cities (
 	id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+	state_id INT NOT NULL,
 	FOREIGN KEY (state_id) REFERENCES states(id),
 	name varchar(256) NOT NULL
 );
