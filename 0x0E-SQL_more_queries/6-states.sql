6a36e0066dd2e470e94c43b3778de597f4c8390d
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-05-22 18:46:35 +0100

    states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- States.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+	name varchar(256)
+);
