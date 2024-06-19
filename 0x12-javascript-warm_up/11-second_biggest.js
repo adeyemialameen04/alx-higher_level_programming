ddb32cd821b3015dd2e20dfb1dc54ebd1475b759
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-19 22:37:26 +0100

    test -2

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,5 +7,7 @@ if (argv.length === 0 || argv.length === 1) {
   const numbers = argv.map(Number);
   const sortedNums = numbers.sort((a, b) => b - a);
 
-  console.log(sortedNums[1]);
+  // console.log(argv[0], typeof (argv[0]))
+  // console.log(argv.sort())
+  console.log(sortedNums[argv.length - 2]);
 }
