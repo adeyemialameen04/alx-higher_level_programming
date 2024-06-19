ffb0d6a5bb19d4f469524c2e4379db8bccd102e7
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-19 15:07:05 +0100

    semistandard and perms

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
 const args = process.argv.slice(2);
-const number = Number(args[0])
-if (isNaN(number)){
-  console.log("Not a number")
-}
-else {
-  console.log(`My number: ${number}`)
+const number = Number(args[0]);
+if (isNaN(number)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${number}`);
 }
