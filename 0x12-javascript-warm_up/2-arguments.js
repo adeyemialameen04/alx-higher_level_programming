54fa8ef44604f9baf25b16a946705123b3d15c54
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-19 13:14:43 +0100

    semistandard --fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-if (argv.length == 1)
-{
-  console.log("No argument")
-}
-else if (argv.length == 2)
-{
-  console.log("Argument found");
-}
-else {
-  console.log("Arguments found")
+if (argv.length === 1) {
+  console.log('No argument');
+} else if (argv.length === 2) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
