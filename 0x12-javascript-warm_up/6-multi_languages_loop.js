fa1db5f67a47c71f0853a5db6ddf87680d1a0cbd
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-19 15:25:12 +0100

    Why?

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 
-const sentences = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const sentences = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
 
 for (const language of sentences) {
   console.log(language);
