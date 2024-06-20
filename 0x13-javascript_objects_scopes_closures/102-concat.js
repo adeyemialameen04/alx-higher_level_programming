1353303e6822b0ac795a0423e5ecee0a8ea51eea
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-20 14:03:38 +0100

    concat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,3 +1,13 @@
 #!/usr/bin/node
 
+const fs = require('fs')
+const argv = process.argv.slice(2);
+const fileC = argv[2]
+const fileA = argv[0]
+const fileB = argv[1]
 
+const dataA = fs.readFileSync(fileA, 'utf-8');
+
+const dataB = fs.readFileSync(fileB, 'utf-8');
+
+fs.writeFileSync(fileC, dataA + dataB);
