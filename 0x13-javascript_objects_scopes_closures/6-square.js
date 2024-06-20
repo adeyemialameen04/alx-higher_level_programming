cf53b9b68ab5670af5b0cb7295d89e789d7a3dc0
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-20 12:51:18 +0100

    2

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const SquareBase = require('./5-square');
+
+class Square extends SquareBase {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    if (this.width && this.height) {
+      let result = "";
+      for (let i = 0; i < this.height; i++) {
+        result += c.repeat(this.width);
+        if (i < this.height - 1) {
+          result += "\n";
+        }
+      }
+      console.log(result);
+    }
+  }
+}
+
+module.exports = Square;
--- 0x13-javascript_objects_scopes_closures/main.js
+++ 0x13-javascript_objects_scopes_closures/main.js
@@ -1,7 +1,5 @@
-#!/usr/bin/node
-const Square = require('./5-square');
+const nbOccurences = require('./7-occurrences').nbOccurences;
 
-const s1 = new Square(4);
-s1.print();
-s1.double();
-s1.print();
+console.log(nbOccurences([1, 2, 3, 4, 5, 6], 3));
+console.log(nbOccurences([3, 2, 3, 4, 5, 3, 3], 3));
+console.log(nbOccurences(["S", 12, "c", "S", "School", 8], "S"));
