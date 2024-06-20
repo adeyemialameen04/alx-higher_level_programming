65628bd6bf2fe6964648c63b95f274b254dac96a
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-20 13:22:50 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,6 +2,6 @@
 
 const list = require('./100-data.js').list;
 
-const newList = list.map((item) => item * 2);
+const newList = list.map((item, idx) => item * idx);
 console.log(list);
 console.log(newList);
