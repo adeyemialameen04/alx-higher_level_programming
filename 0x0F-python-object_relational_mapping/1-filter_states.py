bc95da8bd8f9909a1d3cce0e4ef566d7e5309664
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-23 00:45:07 +0100

    1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""1"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db = argv[3]
+
+    conn = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        passwd=password,
+        db=db,
+        port=3306
+    )
+    c = conn.cursor()
+    c.execute("SELLECT * FROM state WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    conn.close()
