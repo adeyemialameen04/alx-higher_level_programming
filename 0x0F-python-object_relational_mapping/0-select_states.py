ad17f6731eb7c0bfe82c80a1a17797c49f361cdc
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-23 00:10:47 +0100

    0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""0"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    username = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    conn = MySQLdb.connect(
+        username,
+        passwd,
+        db
+    )
+    c = conn.cursor
+    c.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    conn.close()
