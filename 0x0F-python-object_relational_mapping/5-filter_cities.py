bee2a0c33bbda159f97318db784c96e06c4a6062
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-23 02:51:49 +0100

    5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""5"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    username, password, db, state_name = argv[1], argv[2], argv[3], argv[4]
+    conn = MySQLdb.connect(user=username, passwd=password, db=db)
+    c = conn.cursor()
+
+    query = """
+    SELECT cities.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY citiesss.id ASC
+    """
+
+    c.execute(query, (state_name,))
+    rows = c.fetchall()
+    city_names = [row[0] for row in rows]
+    print(", ".join(city_names))
+
+    c.close()
+    conn.close()
