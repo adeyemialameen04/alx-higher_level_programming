113038531ca90d29bf1c10989cc654c9ca2f2fa8
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-23 04:26:33 +0100

    finished mandatory tasks

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Start link class to table in database
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(bind=engine)
+    session = Session(bind=engine)
+
+    # delete all the object containing a
+    result = session.query(State).filter(State.name.like("%a%"))
+    for res in result.all():
+        session.delete(res)
+    session.commit()
+    session.close()
--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Start link class to table in database
+    this is a good module
+"""
+from sys import argv
+from model_state import Base, State
+from model_city import City
+from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(bind=engine)
+    session = Session(bind=engine)
+
+    city = session.query(State, City).join(City).order_by(City.id)
+    for state, city in city:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
