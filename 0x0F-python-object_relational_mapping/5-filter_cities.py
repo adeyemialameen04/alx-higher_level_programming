2732833c34425436a061f5c50f00809de5c9ba55
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-23 02:53:49 +0100

    this typos are getting too much

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     FROM cities
     JOIN states ON cities.state_id = states.id
     WHERE states.name = %s
-    ORDER BY citiesss.id ASC
+    ORDER BY cities.id ASC
     """
 
     c.execute(query, (state_name,))
