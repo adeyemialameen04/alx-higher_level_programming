835be43f1fef14adf8bb1d46aee3154b9896249f
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-07-05 21:19:31 +0100

    comment

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+# comment
 
-# Check if the URL is provided as an argument
 if [ $# -eq 0 ]; then
 	echo "Usage: $0 <URL>"
 	exit 1
@@ -8,8 +8,6 @@ fi
 
 URL=$1
 
-# Send a request to the URL and store the response body size in bytes
 size=$(curl -s -o /dev/null -w '%{size_download}' "$URL")
 
-# Display the size
 echo $size
