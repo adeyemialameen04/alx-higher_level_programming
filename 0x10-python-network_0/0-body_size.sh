3964c115e3ec69ce84ff144e52d4d3e00d64563e
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-07-05 21:21:20 +0100

    3lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 # comment
-
-if [ $# -eq 0 ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
-URL=$1
-
-size=$(curl -s -o /dev/null -w '%{size_download}' "$URL")
-
-echo $size
+curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
